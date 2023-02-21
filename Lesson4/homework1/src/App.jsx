ea4ee343331d4292ab3fee9baf0a19f7d7bb0f06
Dima Bondariev <dima.bondariev@gmail.com>
2023-02-21 11:21:03 +0200

    lesson4 task 1

---
+++ Lesson4/homework1/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => {
+  return <div>
+    <h1>Search Form</h1>
+    <div>
+      <input type="text" />
+      <button>Search</button>
+    </div>
+  </div>;
+};
+
+export default App;
+
+
+{/* <h1>Search Form</h1>
+<div>
+  <input type="text" />
+  <button>Search</button>
+</div> */}
